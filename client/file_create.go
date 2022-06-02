d7ca26e67f4723aaeaa8bee3fe003c40b549303b
Douglas Parsons <dglsparsons@users.noreply.github.com>
2022-06-02 12:20:17 +0100

    Fix permissions issue with uploading deployment files (#42)

--- client/file_create.go
+++ client/file_create.go
@@ -9,26 +9,35 @@ import (
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
+type CreateFileRequest struct {
+	Filename string
+	SHA      string
+	Content  string
+	TeamID   string
+}
+
 // CreateFile will upload a file to Vercel so that it can be later used for a Deployment.
-func (c *Client) CreateFile(ctx context.Context, filename, sha, content string) error {
+func (c *Client) CreateFile(ctx context.Context, request CreateFileRequest) error {
 	url := fmt.Sprintf("%s/v2/now/files", c.baseURL)
+	if request.TeamID != "" {
+		url = fmt.Sprintf("%s?teamId=%s", url, request.TeamID)
+	}
 	req, err := http.NewRequestWithContext(
 		ctx,
 		"POST",
 		url,
-		strings.NewReader(content),
+		strings.NewReader(request.Content),
 	)
 	if err != nil {
 		return err
 	}
 
-	req.Header.Add("x-vercel-digest", sha)
+	req.Header.Add("x-vercel-digest", request.SHA)
 	req.Header.Set("Content-Type", "application/octet-stream")
 
 	tflog.Trace(ctx, "uploading file", map[string]interface{}{
-		"url":     url,
-		"payload": mustMarshal(content),
-		"sha":     sha,
+		"url": url,
+		"sha": request.SHA,
 	})
 	err = c.doRequest(req, nil)
 	return err
