f464b2d4786b52ed181aadc6586607bad589f648
Douglas Harcourt Parsons <dglsparsons@gmail.com>
2024-01-17 12:21:38 +0000

    Update wording of env var / dns record IDs to make it clearer
    There is no network tab in the Vercel UI, this is referring to developer
    tools.

--- docs/resources/shared_environment_variable.md
+++ docs/resources/shared_environment_variable.md
@@ -65,6 +65,6 @@ Import is supported using the following syntax:
 ```shell
 # You can import via the team_id and environment variable id.
 # - team_id can be found in the team `settings` tab in the Vercel UI.
-# - environment variable id can be taken from the network tab on the shared environment variable page.
+# - environment variable id is hard to find, but can be taken from the network tab, inside developer tools, on the shared environment variable page.
 terraform import vercel_shared_environment_variable.example team_xxxxxxxxxxxxxxxxxxxxxxxx/env_yyyyyyyyyyyyy
 ```
--- examples/resources/vercel_dns_record/import.sh
+++ examples/resources/vercel_dns_record/import.sh
@@ -1,10 +1,10 @@
 # If importing into a personal account, or with a team configured on
 # the provider, simply use the record id.
-# - record_id can be taken from the network tab on the domains page.
+# - record_id is hard to find, but can be taken from the network tab, inside developer tools, on the domains page.
 terraform import vercel_dns_record.example rec_xxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 # Alternatively, you can import via the team_id and record_id.
 # - team_id can be found in the team `settings` tab in the Vercel UI.
-# - record_id can be taken from the network tab on the domains page.
+# - record_id is hard to find, but can be taken from the network tab, inside developer tools, on the domains page.
 terraform import vercel_dns_record.example team_xxxxxxxxxxxxxxxxxxxxxxxx/rec_xxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
