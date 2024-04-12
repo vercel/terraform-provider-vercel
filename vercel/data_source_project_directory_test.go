f1df75e0b0590c640bddc1f0d20b3123e060a63b
Douglas Harcourt Parsons <dglsparsons@gmail.com>
2024-04-12 15:02:04 +0100

    Upgrade all dependencies, especially testing ones

--- vercel/data_source_project_directory_test.go
+++ vercel/data_source_project_directory_test.go
@@ -4,7 +4,7 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAcc_DataSourceProjectDirectory(t *testing.T) {
