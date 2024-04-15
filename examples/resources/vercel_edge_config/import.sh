686bda3108d3c125b749d3765882382b3532a448
Douglas Harcourt Parsons <dglsparsons@users.noreply.github.com>
2024-04-15 12:30:14 +0100

    Correct edge-config-id references (#163)

--- examples/resources/vercel_edge_config/import.sh
+++ examples/resources/vercel_edge_config/import.sh
@@ -1,9 +1,9 @@
 # If importing into a personal account, or with a team configured on
 # the provider, simply use the edge config id.
-# - edge_config_id is hard to find, but can be found by navigating to the Edge Config in the Vercel UI and looking at the URL. It should begin with `ecfg_`.
+# - edge_config_id can be found by navigating to the Edge Config in the Vercel UI. It should begin with `ecfg_`.
 terraform import vercel_edge_config.example ecfg_xxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 # Alternatively, you can import via the team_id and edge_config_id.
 # - team_id can be found in the team `settings` tab in the Vercel UI.
-# - edge_config_id is hard to find, but can be found by navigating to the Edge Config in the Vercel UI and looking at the URL. It should begin with `ecfg_`.
+# - edge_config_id can be found by navigating to the Edge Config in the Vercel UI. It should begin with `ecfg_`.
 terraform import vercel_edge_config.example team_xxxxxxxxxxxxxxxxxxxxxxxx/ecfg_xxxxxxxxxxxxxxxxxxxxxxxxxxxx
