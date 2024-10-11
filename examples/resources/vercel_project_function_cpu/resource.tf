97d3750daca3c4bcaddb02b8ff17258a4a8be163
Douglas Harcourt Parsons <dglsparsons@gmail.com>
2024-10-11 10:46:14 +0100

    Remove deprecated and non-function project_function_cpu resource + data source

--- examples/resources/vercel_project_function_cpu/resource.tf
+++
@@ -1,8 +0,0 @@
-resource "vercel_project" "example" {
-  name = "example"
-}
-
-resource "vercel_project_function_cpu" "example" {
-  project_id = vercel_project.example.id
-  cpu        = "performance"
-}
