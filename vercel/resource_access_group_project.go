1db2a966900472285ef617955a9c6614e23f0931
Marc Greenstock <marc@marcgreenstock.com>
2024-11-18 15:36:27 +0100

    Update resource_access_group_project.go

--- vercel/resource_access_group_project.go
+++ vercel/resource_access_group_project.go
@@ -31,7 +31,7 @@ type accessGroupProjectResource struct {
 }
 
 func (r *accessGroupProjectResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = req.ProviderTypeName + "_log_drain"
+	resp.TypeName = req.ProviderTypeName + "_access_group_project"
 }
 
 func (r *accessGroupProjectResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
