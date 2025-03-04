f01edf7d2f29ba74116b1310c1dbc35b7012433e
Kit Foster <katya.a.foster@gmail.com>
2025-03-04 10:58:49 +0100

    Fix microfrontends example

--- examples/resources/vercel_microfrontend_group/resource.tf
+++ examples/resources/vercel_microfrontend_group/resource.tf
@@ -7,13 +7,10 @@ data "vercel_project" "child_mfe_project" {
 }
 
 resource "vercel_microfrontend_group" "example_mfe_group" {
-  name        = "my mfe"
-  default_app = vercel_project.parent_mfe_project.id
-}
-
-resource "vercel_microfrontend_group_membership" "parent_mfe_project_mfe_membership" {
-  project_id             = vercel_project.parent_mfe_project.id
-  microfrontend_group_id = vercel_microfrontend_group.example_mfe_group.id
+  name = "my mfe"
+  default_app = {
+    project_id = data.vercel_project.parent_mfe_project.id
+  }
 }
 
 resource "vercel_microfrontend_group_membership" "child_mfe_project_mfe_membership" {
