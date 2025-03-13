9bde3a7c56ec1d8716738599dc3436b66cb49aaa
sueplex <sage.abraham@vercel.com>
2025-03-13 10:26:57 -0400

    fix broken test config

--- vercel/resource_firewall_config_test.go
+++ vercel/resource_firewall_config_test.go
@@ -651,7 +651,7 @@ resource "vercel_firewall_config" "custom_neg" {
                     "3.4.5.6",
                     "5.6.7.7",
                 ]
-            }
+            }]
           }]
         }
     }
