ed0fef6f5ade361927ea0227fc334e174b8bb3a7
Brooke Mosby <brookemosby@icloud.com>
2025-06-13 15:56:25 -0600

    [rr]

--- vercel/resource_project_rolling_release_test.go
+++ vercel/resource_project_rolling_release_test.go
@@ -132,6 +132,7 @@ func testAccProjectRollingReleasesConfigUpdated(nameSuffix string) string {
 	return fmt.Sprintf(`
 resource "vercel_project" "example" {
 	name = "test-acc-rolling-releases-%s"
+	skew_protection = "12 hours"
 }
 
 resource "vercel_project_rolling_release" "example" {
