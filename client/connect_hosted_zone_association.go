954acad346413b2b18e2566adab32048af673d02
Yanick Bélanger <yanick.belanger@yahoo.com>
2025-07-30 16:08:12 -0400

    add client methods for hosted zone associations

---
+++ client/connect_hosted_zone_association.go
@@ -0,0 +1,95 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+type HostedZoneAssociation struct {
+	HostedZoneID   string `json:"hostedZoneId"`
+	HostedZoneName string `json:"hostedZoneName"`
+	Owner          string `json:"owner"`
+}
+
+type GetHostedZoneAssociationRequest struct {
+	ConfigurationID string
+	HostedZoneID    string
+	TeamID          string
+}
+
+func (c *Client) GetHostedZoneAssociation(ctx context.Context, req GetHostedZoneAssociationRequest) (r HostedZoneAssociation, err error) {
+	url := fmt.Sprintf("%s/v1/connect/configurations/%s/hosted-zones/%s", c.baseURL, req.ConfigurationID, req.HostedZoneID)
+	if c.TeamID(req.TeamID) != "" {
+		url = fmt.Sprintf("%s?teamId=%s", url, c.TeamID(req.TeamID))
+	}
+
+	tflog.Info(ctx, "Getting Hosted Zone Association", map[string]any{"url": url})
+
+	err = c.doRequest(clientRequest{ // TODO: This endpoint actually returns a different shape, we should change it to return HostedZoneAssociation
+		body:   "",
+		ctx:    ctx,
+		method: "GET",
+		url:    url,
+	}, &r)
+
+	return r, err
+}
+
+type CreateHostedZoneAssociationRequest struct {
+	ConfigurationID string
+	HostedZoneID    string
+	TeamID          string
+}
+
+func (c *Client) CreateHostedZoneAssociation(ctx context.Context, req CreateHostedZoneAssociationRequest) (r HostedZoneAssociation, err error) {
+	url := fmt.Sprintf("%s/v1/connect/configurations/%s/hosted-zones", c.baseURL, req.ConfigurationID)
+	if c.TeamID(req.TeamID) != "" {
+		url = fmt.Sprintf("%s?teamId=%s", url, c.TeamID(req.TeamID))
+	}
+
+	body := string(mustMarshal(
+		struct {
+			HostedZoneId string `json:"hostedZoneId"`
+		}{
+			HostedZoneId: req.HostedZoneID,
+		},
+	))
+
+	tflog.Info(ctx, "Creating Hosted Zone Association", map[string]any{
+		"body": body,
+		"url":  url,
+	})
+
+	err = c.doRequest(clientRequest{
+		body:   body,
+		ctx:    ctx,
+		method: "POST",
+		url:    url,
+	}, &r)
+
+	return r, err
+}
+
+type DeleteHostedZoneAssociationRequest struct {
+	ConfigurationID string
+	HostedZoneID    string
+	TeamID          string
+}
+
+func (c *Client) DeleteHostedZoneAssociation(ctx context.Context, req DeleteHostedZoneAssociationRequest) error {
+	url := fmt.Sprintf("%s/v1/connect/configurations/%s/hosted-zones/%s", c.baseURL, req.ConfigurationID, req.HostedZoneID)
+	if c.TeamID(req.TeamID) != "" {
+		url = fmt.Sprintf("%s?teamId=%s", url, c.TeamID(req.TeamID))
+	}
+
+	tflog.Info(ctx, "Deleting Hosted Zone Association", map[string]any{"url": url})
+
+	return c.doRequest(clientRequest{
+		body:   "",
+		ctx:    ctx,
+		method: "DELETE",
+		url:    url,
+	}, nil)
+}
