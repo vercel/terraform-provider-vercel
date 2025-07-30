bf2d650ad3cd4c379690360356bc95105ee12093
Yanick Bélanger <yanick.belanger@yahoo.com>
2025-07-30 15:13:33 -0400

    add boilerplate

---
+++ vercel/resource_hosted_zone_association.go
@@ -0,0 +1,59 @@
+package vercel
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/vercel/terraform-provider-vercel/v3/client"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ resource.Resource                = &hostedZoneAssociationResource{}
+	_ resource.ResourceWithConfigure   = &hostedZoneAssociationResource{}
+	_ resource.ResourceWithImportState = &hostedZoneAssociationResource{}
+)
+
+type hostedZoneAssociationResource struct {
+	client *client.Client
+}
+
+// Configure implements resource.ResourceWithConfigure.
+func (h *hostedZoneAssociationResource) Configure(context.Context, resource.ConfigureRequest, *resource.ConfigureResponse) {
+	panic("unimplemented")
+}
+
+// Create implements resource.Resource.
+func (h *hostedZoneAssociationResource) Create(context.Context, resource.CreateRequest, *resource.CreateResponse) {
+	panic("unimplemented")
+}
+
+// Delete implements resource.Resource.
+func (h *hostedZoneAssociationResource) Delete(context.Context, resource.DeleteRequest, *resource.DeleteResponse) {
+	panic("unimplemented")
+}
+
+// ImportState implements resource.ResourceWithImportState.
+func (h *hostedZoneAssociationResource) ImportState(context.Context, resource.ImportStateRequest, *resource.ImportStateResponse) {
+	panic("unimplemented")
+}
+
+// Metadata implements resource.Resource.
+func (h *hostedZoneAssociationResource) Metadata(context.Context, resource.MetadataRequest, *resource.MetadataResponse) {
+	panic("unimplemented")
+}
+
+// Read implements resource.Resource.
+func (h *hostedZoneAssociationResource) Read(context.Context, resource.ReadRequest, *resource.ReadResponse) {
+	panic("unimplemented")
+}
+
+// Schema implements resource.Resource.
+func (h *hostedZoneAssociationResource) Schema(context.Context, resource.SchemaRequest, *resource.SchemaResponse) {
+	panic("unimplemented")
+}
+
+// Update implements resource.Resource.
+func (h *hostedZoneAssociationResource) Update(context.Context, resource.UpdateRequest, *resource.UpdateResponse) {
+	panic("unimplemented")
+}
