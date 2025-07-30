029935ea30781c4db1679a9c7d903168b04f28b8
Yanick Bélanger <yanick.belanger@yahoo.com>
2025-07-30 15:15:37 -0400

    implement Configure

--- vercel/resource_hosted_zone_association.go
+++ vercel/resource_hosted_zone_association.go
@@ -2,6 +2,7 @@ package vercel
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/vercel/terraform-provider-vercel/v3/client"
@@ -18,41 +19,54 @@ type hostedZoneAssociationResource struct {
 	client *client.Client
 }
 
-// Configure implements resource.ResourceWithConfigure.
-func (h *hostedZoneAssociationResource) Configure(context.Context, resource.ConfigureRequest, *resource.ConfigureResponse) {
-	panic("unimplemented")
+func (r *hostedZoneAssociationResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*client.Client)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *client.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+
+	r.client = client
 }
 
 // Create implements resource.Resource.
-func (h *hostedZoneAssociationResource) Create(context.Context, resource.CreateRequest, *resource.CreateResponse) {
+func (r *hostedZoneAssociationResource) Create(context.Context, resource.CreateRequest, *resource.CreateResponse) {
 	panic("unimplemented")
 }
 
 // Delete implements resource.Resource.
-func (h *hostedZoneAssociationResource) Delete(context.Context, resource.DeleteRequest, *resource.DeleteResponse) {
+func (r *hostedZoneAssociationResource) Delete(context.Context, resource.DeleteRequest, *resource.DeleteResponse) {
 	panic("unimplemented")
 }
 
 // ImportState implements resource.ResourceWithImportState.
-func (h *hostedZoneAssociationResource) ImportState(context.Context, resource.ImportStateRequest, *resource.ImportStateResponse) {
+func (r *hostedZoneAssociationResource) ImportState(context.Context, resource.ImportStateRequest, *resource.ImportStateResponse) {
 	panic("unimplemented")
 }
 
-func (h *hostedZoneAssociationResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+func (r *hostedZoneAssociationResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
 	resp.TypeName = req.ProviderTypeName + "_hosted_zone_association"
 }
 
 // Read implements resource.Resource.
-func (h *hostedZoneAssociationResource) Read(context.Context, resource.ReadRequest, *resource.ReadResponse) {
+func (r *hostedZoneAssociationResource) Read(context.Context, resource.ReadRequest, *resource.ReadResponse) {
 	panic("unimplemented")
 }
 
 // Schema implements resource.Resource.
-func (h *hostedZoneAssociationResource) Schema(context.Context, resource.SchemaRequest, *resource.SchemaResponse) {
+func (r *hostedZoneAssociationResource) Schema(context.Context, resource.SchemaRequest, *resource.SchemaResponse) {
 	panic("unimplemented")
 }
 
 // Update implements resource.Resource.
-func (h *hostedZoneAssociationResource) Update(context.Context, resource.UpdateRequest, *resource.UpdateResponse) {
+func (r *hostedZoneAssociationResource) Update(context.Context, resource.UpdateRequest, *resource.UpdateResponse) {
 	panic("unimplemented")
 }
