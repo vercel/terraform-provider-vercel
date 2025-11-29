a8e8baba3362ee794dd8f076873a5d34e9943579
Corey <corey@livepeer.org>
2025-11-28 16:36:57 -0800

    abandon project_environment_variables

--- vercel/provider.go
+++ vercel/provider.go
@@ -74,7 +74,6 @@ func (p *vercelProvider) Resources(_ context.Context) []func() resource.Resource
 		newProjectDomainResource,
 		newProjectEnvironmentVariableResource,
 		newProjectEnvironmentVariablesResource,
-		newProjectEnvironmentVariablesV2Resource,
 		newProjectMembersResource,
 		newProjectResource,
 		newSharedEnvironmentVariableProjectLinkResource,
