e0d2324d3f284cf592af264036f0a5e84b6761ac
Corey <corey@livepeer.org>
2025-11-28 16:03:36 -0800

    can't use WO values in nested sets

--- vercel/resource_project.go
+++ vercel/resource_project.go
@@ -956,6 +956,7 @@ type EnvironmentItem struct {
 	GitBranch            types.String `tfsdk:"git_branch"`
 	Key                  types.String `tfsdk:"key"`
 	Value                types.String `tfsdk:"value"`
+	ValueWO              types.String `tfsdk:"value_wo"`
 	ID                   types.String `tfsdk:"id"`
 	Sensitive            types.Bool   `tfsdk:"sensitive"`
 	Comment              types.String `tfsdk:"comment"`
@@ -964,6 +965,7 @@ type EnvironmentItem struct {
 func (e *EnvironmentItem) equal(other *EnvironmentItem) bool {
 	return e.Key.ValueString() == other.Key.ValueString() &&
 		e.Value.ValueString() == other.Value.ValueString() &&
+		e.ValueWO.ValueString() == other.ValueWO.ValueString() &&
 		e.Target.Equal(other.Target) &&
 		e.CustomEnvironmentIDs.Equal(other.CustomEnvironmentIDs) &&
 		e.GitBranch.ValueString() == other.GitBranch.ValueString() &&
@@ -976,6 +978,7 @@ func (e *EnvironmentItem) toAttrValue() attr.Value {
 		"id":                     e.ID,
 		"key":                    e.Key,
 		"value":                  e.Value,
+		"value_wo":               e.ValueWO,
 		"target":                 e.Target,
 		"custom_environment_ids": e.CustomEnvironmentIDs,
 		"git_branch":             e.GitBranch,
