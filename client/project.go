297da9bf58cdd185c2c8cfb98a0f79a0cfd79ecc
Douglas Harcourt Parsons <dglsparsons@gmail.com>
2026-06-29 20:17:44 +0100

    deprecate public_source project field

--- client/project.go
+++ client/project.go
@@ -55,7 +55,6 @@ type CreateProjectRequest struct {
 	OIDCTokenConfig                   *OIDCTokenConfig      `json:"oidcTokenConfig,omitempty"`
 	OutputDirectory                   *string               `json:"outputDirectory"`
 	PreviewDeploymentSuffix           *string               `json:"previewDeploymentSuffix"`
-	PublicSource                      *bool                 `json:"publicSource"`
 	RootDirectory                     *string               `json:"rootDirectory"`
 	ServerlessFunctionRegion          string                `json:"serverlessFunctionRegion,omitempty"`
 	ResourceConfig                    *ResourceConfig       `json:"resourceConfig,omitempty"`
